a935a70221150f42c93ea55b88ef0cff8248f5e4
Jared Delora-Ellefson <jareddelora@gmail.com>
2020-07-09 13:20:08 -0400

    TextManager.py in process...

--- spacy/TextManager.py
+++ spacy/TextManager.py
@@ -7,21 +7,36 @@ class TextRunner:
         self.df = pd.DataFrame()
         self.df_out = pd.DataFrame()
 
-    def Cleaner(self):
-        self.df = pd.read_csv('./training_data/collected_term_comments.csv')
+    def _Cleaner(self, file):
+        self.df = pd.read_csv(file)
         cleaned = []
-        for item in self.df['text']:
-            text = item.replace("'", "")
-            text = text.replace("\n", "")
-            cleaned.append(text)
-        self.df['text'] = cleaned
-        self.df['text'] = self.df['text'].str.lower().apply(lambda x: re.sub("[^a-z\s]", " ", x))
-        self.df_out['text'] = self.df['text']
-        self.df_out.to_json('./training_data/cleaned_training_data.jsonl', orient='records', lines=True)
+        curr = 1
+        for item in self.df['body']:
 
-    def Explorer(self):
+            try:
+                text = item.replace("'", "")
+                text = text.replace("\n", "")
+                cleaned.append(text)
+            except:
+                print(f'Skipping line {curr}')
+
+            curr += 1
+
+        self.df_out['text'] = cleaned
+        self.df_out['text'] = self.df_out['text'].str.lower().apply(lambda x: re.sub("[^a-z\s]", " ", x))
+        self.df_out['text'].dropna(inplace=True)
+        return self.df_out
+        # self.df_out.to_json('./training_data/cleaned_training_data.jsonl', orient='records', lines=True)
+
+    def SpacyPrinter(self):
         self.df = pd.read_json('./CON_patters.jsonl', orient='records', lines=True)
         print(self.df.head())
+
+    def Explorer(self):
+        file = './the_donald/bq-results-1.csv'
+        self.df = self._Cleaner(file)
+        print(self.df.head())
+
 
 
 
